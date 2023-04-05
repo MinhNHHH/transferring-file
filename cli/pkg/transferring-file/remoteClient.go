93fa3489e0e54104cd7d91b16785d760e9029c66
Nguyễn Hoàng Minh 79 (AI) <minhnh79@fpt.com.vn>
2023-04-05 17:00:35 +0700

    z

--- cli/pkg/transferring-file/remoteClient.go
+++ cli/pkg/transferring-file/remoteClient.go
@@ -1,6 +1,11 @@
 package transferringfile
 
 import (
+	"encoding/json"
+	"log"
+	"webrtc/cli/pkg/message"
+	"webrtc/internal/cfg"
+
 	"github.com/google/uuid"
 	"github.com/pion/webrtc/v3"
 )
@@ -10,7 +15,7 @@ type RemoteClient struct {
 
 	// use Client struct
 	// for transferring terminal changes
-	datachannel   *webrtc.DataChannel
+	dataChannel   *webrtc.DataChannel
 	configChannel *webrtc.DataChannel
 	peerConn      *webrtc.PeerConnection
 	wsConn        *Websocket
@@ -25,4 +30,58 @@ func NewRemoteClient() *RemoteClient {
 		connected: false,
 		done:      make(chan bool),
 	}
+}
+
+func (rc *RemoteClient) Connect(server string, sessionID string) {
+	log.Printf("Start")
+	wsURL := "ws://localhost:8080/ws"
+
+	wsConn, err := NewWebSocketConnection(wsURL)
+	if err != nil {
+		log.Printf("Failed to connect to singaling server : %s", err)
+	}
+	go wsConn.Start()
+
+	// Initiate peer to peer
+	iceServers := cfg.TERMISHARE_ICE_SERVER_STUNS
+	iceServers = append(iceServers, cfg.TERMISHARE_ICE_SERVER_TURNS...)
+
+	config := webrtc.Configuration{
+		ICEServers:   iceServers,
+		SDPSemantics: webrtc.SDPSemanticsUnifiedPlanWithFallback,
+	}
+
+	peerConn, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		log.Printf("Failed to create peer connetion : %s", err)
+	}
+
+	rc.peerConn = peerConn
+
+	peerConn.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		log.Printf("Peer connection state has changed: %s", s.String())
+		switch s {
+		case webrtc.PeerConnectionStateClosed:
+		case webrtc.PeerConnectionStateDisconnected:
+		case webrtc.PeerConnectionStateFailed:
+			rc.peerConn.Close()
+			log.Printf("Disconnected")
+		}
+	})
+
+	configChannel, err := peerConn.CreateDataChannel("config", nil)
+	dataChannel, err := peerConn.CreateDataChannel("transfer", nil)
+	rc.configChannel = configChannel
+	rc.dataChannel = dataChannel
+
+	configChannel.OnMessage(func(webrtcMsg webrtc.DataChannelMessage) {
+		msg := &message.Wrapper{}
+		err := json.Unmarshal(webrtcMsg.Data, msg)
+		if err != nil {
+			log.Printf("Failed to read config message: %s", err)
+			return
+		}
+		log.Printf("Config channel got msg: %v", msg)
+
+	})
 }
--- main.go
+++ main.go
@@ -1,69 +1,16 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/websocket"
+	transferringfile "webrtc/cli/pkg/transferring-file"
 )
 
 type message struct {
-	Type string `json:"type"`
-	Data string `json:"data"`
+	Type string
+	Data interface{}
 }
-
-var (
-	upgrader = websocket.Upgrader{
-		// ReadBufferSize:  1024,
-		// WriteBufferSize: 1024,
-		// CheckOrigin: func(r *http.Request) bool {
-		// 	return true
-		// },
-	}
-
-	clients = make(map[*websocket.Conn]bool)
-)
 
 func main() {
-	http.HandleFunc("/", handleWebSocket)
-	err := http.ListenAndServe(":8080", nil)
-	log.Println("Listening Sever port: 8080")
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
+	rc := transferringfile.NewRemoteClient()
 
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	// Upgrade HTTP connection to WebSocket
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("Error upgrading WebSocket connection:", err)
-		return
-	}
-	defer conn.Close()
-
-	// Add new client to clients map
-	clients[conn] = true
-
-	for {
-		// Read incoming message from client
-		var msg message
-		err := conn.ReadJSON(&msg)
-		if err != nil {
-			log.Println("Error reading JSON message from client:", err)
-			delete(clients, conn)
-			break
-		}
-		// Broadcast message to all connected clients
-		for client := range clients {
-			if client != conn {
-				err := client.WriteJSON(msg)
-				if err != nil {
-					log.Println("Error writing JSON message to client:", err)
-					delete(clients, client)
-					break
-				}
-			}
-		}
-	}
+	rc.Connect("localhost:8080", "1")
 }
